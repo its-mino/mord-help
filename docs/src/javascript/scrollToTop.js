de43280ee57c6b9f641526a2e8bb2583aeedecd3
Brendan Sileo <brendansileo@gmail.com>
2023-01-26 13:35:46 -0500

    moved everything to root so it's cleaner

--- docs/src/javascript/scrollToTop.js
+++
@@ -1,5 +0,0 @@
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTo = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
